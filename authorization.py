aaa7ecccef3b67d6b84ba6d5bc6810e25c4c8195
houxh <houxuehua49@gmail.com>
2014-07-26 11:05:54 +0800

    add authorization

---
+++ authorization.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from flask import request
+from functools import wraps
+from model import token
+from datetime import datetime
+
+rds = None
+
+def INVALID_ACCESS_TOKEN():
+    e = {"error":"非法的access token"}
+    return make_response(400, e)
+def EXPIRE_ACCESS_TOKEN():
+    e = {"error":"过期的access token"}
+    return make_response(400, e)
+
+def require_auth(f):
+    """Protect resource with specified scopes."""
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'Authorization' in request.headers:
+            tok = request.headers.get('Authorization')[7:]
+        else:
+            return INVALID_ACCESS_TOKEN()
+        t = token.AccessToken()
+        if not t.load(rds, tok):
+            return INVALID_ACCESS_TOKEN()
+        if datetime.utcnow() > t.expires:
+            print t.expires, datetime.utcnow()
+            return EXPIRE_ACCESS_TOKEN()
+        request.uid = t.user_id
+        return f(*args, **kwargs)
+    return wrapper
