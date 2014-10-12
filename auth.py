c0f910ffc451449d4408306822a2cdd724ec820b
houxh <houxuehua49@gmail.com>
2014-10-12 12:11:23 +0800

    测试账号不校验登录频率

--- auth.py
+++ auth.py
@@ -18,23 +18,28 @@ rds = None
 
 def OVERFLOW():
     e = {"error":"get verify code exceed the speed rate"}
+    logging.warn("get verify code exceed the speed rate")
     return make_response(400, e)
 
 def INVALID_PARAM():
     e = {"error":"非法输入"}
+    logging.warn("非法输入")
     return make_response(400, e)
 
 def INVALID_CODE():
     e = {"error":"验证码错误"}
+    logging.warn("验证码错误")
     return make_response(400, e)
 
 def SMS_FAIL():
     e = {"error":"发送短信失败"}
+    logging.warn("发送短信失败")
     return make_response(400, e)
     
     
 def INVALID_REFRESH_TOKEN():
     e = {"error":"非法的refresh token"}
+    logging.warn("非法的refresh token")
     return make_response(400, e)
     
 def create_verify_code():
@@ -97,7 +102,7 @@ def verify_code():
     zone = request.args.get("zone", "")
     number = request.args.get("number", "")
     logging.info("zone:%s number:%s", zone, number)
-    if not check_verify_rate(zone, number):
+    if not is_test_number(number) and not check_verify_rate(zone, number):
         return OVERFLOW()
         
     vc = create_verify_code()
