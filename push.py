1b42c755d5af120e5e6407f4d352dafc5467ff88
houxh <houxuehua49@gmail.com>
2014-12-12 14:44:27 +0800

    puth_type == 1

--- push.py
+++ push.py
@@ -48,7 +48,7 @@ def ng_push(u, body):
 
     obj = {}
     obj["title"] = u"羊蹄甲"
-    obj["push_type"] = 3
+    obj["push_type"] = 1
     obj["is_ring"] = True
     obj["is_vibrate"] = True
     if content.has_key("text"):
