3c612261d8dd0c89f0c93b6b4d2a92509b08f033
houxh <houxuehua49@gmail.com>
2015-02-10 22:12:06 +0800

    程序配置

--- pro_config/run.sh
+++ pro_config/run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#!/bin/bash
 
-uwsgi=/usr/local/python2.7/bin/uwsgi
-home=/usr/local/python2.7
-app_dir=/vagrant/dev/im_api
+uwsgi=/data/envs/test/bin/uwsgi
+home=/data/envs/test
+app_dir=/data/im_api
 
 $uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5000 -M  -p 1 -w im --callable app -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/im_api.log --pidfile /tmp/im_api.pid --touch-reload /tmp/im_api.touch
 
 $uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5001 --gevent 1000 -M -p 1 -w webapp  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/im_web.log --pidfile /tmp/im_web.pid --touch-reload /tmp/im_web.touch
 
 $uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5002 --gevent 1000 -M  -p 1 -w qr_login  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/qr_login.log --pidfile /tmp/qr_login.pid --touch-reload /tmp/qr_login.touch
-
--- pro_config/stop.sh
+++ pro_config/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uwsgi=/usr/local/python2.7/bin/uwsgi
+uwsgi=/data/envs/test/bin/uwsgi
 
 $uwsgi --stop /tmp/im_api.pid
 $uwsgi --stop /tmp/im_web.pid
