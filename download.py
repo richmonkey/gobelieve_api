3b3fdf92225597ca204eeb54aff99813f8c3ffb9
houxh <houxuehua49@gmail.com>
2015-03-04 16:22:57 +0800

    应用最新版本信息接口

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint
 from flask import redirect
 import httpagentparser
+from util import make_response
 
 app = Blueprint('download', __name__)
 
@@ -26,13 +27,74 @@ def platforms():
     return str_platform
 
 
-@app.route("/download", methods=["GET"])
-def download():
+def is_voip():
+    pos = request.base_url.find("http://voip")
+    return pos == 0
+    
+def is_ios():
     platform = platforms()
     if platform == 'iphone' or platform == 'ipad' or platform == 'ios':
-        package_url = "https://itunes.apple.com/us/app/yang-ti-jia/id923695740?mt=8"
+        return True
     else:
-        package_url = "http://gdown.baidu.com/data/wisegame/1e513446470ee74a/yangtijia_2.apk"
+        return False
+
+def message_package(is_ios):
+    if is_ios:
+        return "https://itunes.apple.com/us/app/yang-ti-jia/id923695740?mt=8"
+    else:
+        return "http://gdown.baidu.com/data/wisegame/1e513446470ee74a/yangtijia_2.apk"
+    
+def face_package(is_ios):
+    if is_ios:
+        return "https://itunes.apple.com/us/app/dian-hua-chong/id939167209?mt=8"
+    else:
+        return "http://gdown.baidu.com/data/wisegame/92a7589f8fe0d204/dianhuachong_1.apk"
+
+@app.route("/download", methods=["GET"])
+def download():
+    if is_voip():
+        package_url = face_package(is_ios())
+    else:
+        package_url = message_package(is_ios())
 
     return redirect(package_url)
 
+def face_app_version(is_ios):
+    ver = {}
+    ver["url"] = face_package(is_ios)
+    if is_ios:
+        ver["major"] = 1
+        ver["minor"] = 0
+    else:
+        ver["major"] = 1
+        ver["minor"] = 0
+    return ver
+
+def message_app_version(is_ios):
+    ver = {}
+    ver["url"] = message_package(is_ios)
+    if is_ios:
+        ver["major"] = 1
+        ver["minor"] = 0
+    else:
+        ver["major"] = 1
+        ver["minor"] = 0
+    return ver
+
+@app.route("/version/android", methods=["GET"])
+def android_version():
+    if is_voip():
+        ver = face_app_version(False)
+    else:
+        ver = message_app_version(False)
+
+    return make_response(200, ver)
+
+@app.route("/version/ios", methods=["GET"])
+def ios_version():
+    if is_voip():
+        ver = face_app_version(True)
+    else:
+        ver = message_app_version(True)
+
+    return make_response(200, ver)
