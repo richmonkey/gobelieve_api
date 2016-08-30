a3e997214eca396bc66f1b92d19b9827a13848e7
houxh <houxuehua49@gmail.com>
2016-08-30 22:29:07 +0800

    add rpc.py

---
+++ views/rpc.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import config
+import logging
+import json
+import requests
+from urllib import urlencode
+
+im_url=config.IM_RPC_URL
+
+
+def post_message(appid, sender, receiver, cls, content):
+    params = {
+        "appid":appid,
+        "class":cls,
+        "sender":sender
+    }
+
+    req_obj = {
+        "receiver":receiver,
+        "content":content,
+    }
+
+    url = im_url + "/post_im_message?" + urlencode(params)
+    logging.debug("url:%s", url)
+    headers = {"Content-Type":"application/json"}
+    res = requests.post(url, data=json.dumps(req_obj), headers=headers)
+    return res
+    
+
+
+def send_group_notification_s(appid, gid, notification, members):
+    url = im_url + "/post_group_notification"
+
+    obj = {
+        "appid": appid,
+        "group_id": gid,
+        "notification":notification
+    }
+    if members:
+        obj["members"] = members
+
+    headers = {"Content-Type":"application/json"}
+
+    data = json.dumps(obj)
+    resp = requests.post(url, data=data, headers=headers)
+    if resp.status_code != 200:
+        logging.warning("send group notification error:%s", resp.content)
+    else:
+        logging.debug("send group notification success:%s", data)
+    return resp
+
+def send_group_notification(appid, gid, op, members):
+    return send_group_notification_s(appid, gid, json.dumps(op), members)
